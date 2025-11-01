e62e2326628b1aa0fd2a8f37391be2b9126bb955
Vijay <vijayp2162@gmail.com>
2025-11-01 09:14:09 +0530

    01-11-2025-update-1-09:13

--- resources/views/admin-master/transaction_master.blade.php
+++ resources/views/admin-master/transaction_master.blade.php
@@ -65,6 +65,7 @@
                                                 </div>
 
                                             </div>
+                                            
                                             <div class="col-lg-12">
 
                                                 <div class="mb-3">
